f646768e519d743d61f57107bbe495e5c26c3738
Chethan M <chethan.marideva@gmail.com>
2020-03-01 15:38:58 +0530

    Inital Project Creation

---
+++ CMKart/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CMKart.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
